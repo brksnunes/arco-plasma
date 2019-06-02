c3617e1b477547d76db188c02b700a0873be0983
¨Rodrigo¨ <rodrigo.nunes792@gmail.com>
2019-06-02 16:32:51 -0300

    Mudei a forma de instalar o plasma

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -20,22 +20,25 @@ echo "Stop the installation with CTRL + C"
 echo "and run the alias - mirror in the terminal"
 echo "#################################################"
 
-echo "Removing xcursor-breeze to avoid conflict with breeze"
-sudo pacman -R xcursor-breeze --noconfirm
+#echo "Removing xcursor-breeze to avoid conflict with breeze"
+#sudo pacman -R xcursor-breeze --noconfirm
 
 sudo pacman -Syyu --noconfirm
+
 #installing displaymanager or login manager
-sudo pacman -S --noconfirm --needed lightdm
-sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter-plasma arcolinux-lightdm-gtk-greeter-settings
-sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git
-sudo pacman -S --noconfirm --needed dolphin konsole
+#sudo pacman -S --noconfirm --needed lightdm
+sudo pacman -S --noconfirm --needed xorg xorg-xinit
+echo "exec startkde" > ~/.xinitrc
+
 #installing desktop environment
-sudo pacman -S plasma --noconfirm --needed
+sudo pacman -S plasma-meta --noconfirm --needed
+sudo pacman -S --noconfirm --needed dolphin konsole
+
+#installing displaymanager or login manager
+sudo pacman -S --noconfirm --needed sddm
+
+
 #enabling displaymanager or login manager
-sudo systemctl enable lightdm.service -f
-sudo systemctl set-default graphical.target
+sudo systemctl enable sddm.service -f
+#sudo systemctl set-default graphical.target
 
-
-#Remove anything you do not like from the installed applications
-
-#sudo pacman -R ...
--- 200-software-arch-linux-repo-v2.sh
+++ 200-software-arch-linux-repo-v2.sh
@@ -20,11 +20,7 @@ set -e
 echo "Installing category Accessories"
 
 sudo pacman -S --noconfirm --needed catfish
-#sudo pacman -S --noconfirm --needed cmatrix
-#sudo pacman -S --noconfirm --needed galculator
-#sudo pacman -S --noconfirm --needed gnome-screenshot
 sudo pacman -S --noconfirm --needed plank
-#sudo pacman -S --noconfirm --needed xfburn
 sudo pacman -S --noconfirm --needed variety
 #sudo pacman -S --noconfirm --needed
 
@@ -36,58 +32,33 @@ sudo pacman -S --noconfirm --needed android-tools
 sudo pacman -S --noconfirm --needed meld
 #sudo pacman -S --noconfirm --needed
 
-echo "Installing category Education"
-
-#sudo pacman -S --noconfirm --needed
-
-echo "Installing category Games"
-
-#sudo pacman -S --noconfirm --needed
-
 echo "Installing category Graphics"
 
-#sudo pacman -S --noconfirm --needed darktable
+sudo pacman -S --noconfirm --needed darktable
 sudo pacman -S --noconfirm --needed gimp
-#sudo pacman -S --noconfirm --needed gnome-font-viewer
-#sudo pacman -S --noconfirm --needed gpick
+sudo pacman -S --noconfirm --needed gnome-font-viewer
+sudo pacman -S --noconfirm --needed gpick
 sudo pacman -S --noconfirm --needed inkscape
 sudo pacman -S --noconfirm --needed nomacs
-#sudo pacman -S --noconfirm --needed pinta
-#sudo pacman -S --noconfirm --needed ristretto
 #sudo pacman -S --noconfirm --needed
 
 echo "Installing category Internet"
 
 sudo pacman -S --noconfirm --needed chromium
-#sudo pacman -S --noconfirm --needed filezilla
 sudo pacman -S --noconfirm --needed firefox
-#sudo pacman -S --noconfirm --needed hexchat
 sudo pacman -S --noconfirm --needed qbittorrent
 #sudo pacman -S --noconfirm --needed
 
 echo "Installing category Multimedia"
 
-#sudo pacman -S --noconfirm --needed clementine
-#sudo pacman -S --noconfirm --needed deadbeef
-#sudo pacman -S --noconfirm --needed mpv
-#sudo pacman -S --noconfirm --needed openshot
-#sudo pacman -S --noconfirm --needed pragha
-#sudo pacman -S --noconfirm --needed shotwell
 sudo pacman -S --noconfirm --needed simplescreenrecorder
-#sudo pacman -S --noconfirm --needed smplayer
 sudo pacman -S --noconfirm --needed vlc
 #sudo pacman -S --noconfirm --needed
 
 echo "Installing category Office"
 
 sudo pacman -S --noconfirm --needed evince
-#sudo pacman -S --noconfirm --needed evolution
-#sudo pacman -S --noconfirm --needed geary
 sudo pacman -S --noconfirm --needed libreoffice-fresh
-#sudo pacman -S --noconfirm --needed
-
-echo "Installing category Other"
-
 #sudo pacman -S --noconfirm --needed
 
 echo "Installing category System"
@@ -109,7 +80,7 @@ sudo pacman -S --noconfirm --needed glances
 #sudo pacman -S --noconfirm --needed gnome-terminal
 #sudo pacman -S --noconfirm --needed gnome-tweak-tool
 sudo pacman -S --noconfirm --needed gparted
-sudo pacman -S --noconfirm --needed grsync
+#sudo pacman -S --noconfirm --needed grsync
 sudo pacman -S --noconfirm --needed gtk-engine-murrine
 sudo pacman -S --noconfirm --needed gvfs gvfs-mtp
 sudo pacman -S --noconfirm --needed hardinfo
